ee6fd8f08de1041f0fd2eaa6c145bad35b2e0789
1cerega10 <r-cergo@yandex.ru>
2021-05-16 12:50:48 +0300

    create: reviews slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const swiper = new Swiper(".swiper-container", {
+const hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
   keyboard: {
     enabled: true,
@@ -25,3 +25,19 @@ function init() {
     zoom: 15,
   });
 }
+
+$(".newsletter").parallax({ imageSrc: "./img/bg-newsletter.jpg" });
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+
+  spaceBetween: 20,
+  autoHeight: true,
+});
